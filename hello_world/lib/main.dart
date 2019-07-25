81625a2cdfe504e6e91cbdd42b1527461e75684d
lidenghong <lidenghong@show-fitness.com>
2019-07-25 11:03:10 +0800

    Container

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -6,16 +6,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'hello_world',
-     home: new Scaffold(
-      // appBar: new AppBar(
-      //   // title: new Text('hello', style: new TextStyle(fontSize: 12),),
-      // ),
-      body: new Center(
-        child: new Text('Hello World', style: new TextStyle(fontSize: 32.0, color: Colors.red, wordSpacing: 10,fontWeight: FontWeight.w400)),
+    // return MaterialApp(
+    //   title: 'hello_world',
+    //   home: new Scaffold(
+    //     // appBar: new AppBar(
+    //     //   // title: new Text('hello', style: new TextStyle(fontSize: 12),),
+    //     // ),
+    //     body: new Center(
+    //       child: new Text('Hello World',
+    //           style: new TextStyle(
+    //               fontSize: 32.0,
+    //               color: Colors.red,
+    //               wordSpacing: 10,
+    //               fontWeight: FontWeight.w400)),
+    //     ),
+    //   ),
+    // );
+    return new Container(
+      decoration: new BoxDecoration(color: Colors.black38),
+      child: new Center(
+        child: new Text(
+          'Hello World', 
+          textDirection: TextDirection.ltr,
+          style: new TextStyle(fontSize: 50),
+        ),
       ),
-    ),
     );
   }
 }
